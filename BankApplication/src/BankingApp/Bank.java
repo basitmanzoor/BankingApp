ec643e5f616e7bb2a0807025804b179a7c44f70e
Basit Manzoor <basit1107@gmail.com>
2023-04-17 00:27:07 +0530

    interface 1

--- BankApplication/src/BankingApp/Bank.java
+++ BankApplication/src/BankingApp/Bank.java
@@ -1,10 +1,8 @@
 package BankingApp;
 
 public interface Bank { //Bank became the common template to be used by other banks
-    //thats why we use interface
+    //that's why we use interface
     public int getBalance();
     public String addMoney(int money, String password, int accountNo);
     public String withdrawMoney(int money, String password);
-
-
 }
